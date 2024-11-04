5da590e2c7a68e3c756520e46cddd9aa0433fb62
MinJae Kim <skwze@naver.com>
2024-11-04 10:28:46 +0900

    Initial commit

---
+++ src/main/java/org/oz/locale24_store_back/LocalE24StoreBackApplication.java
@@ -0,0 +1,13 @@
+package org.oz.locale24_store_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalE24StoreBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocalE24StoreBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/oz/locale24_store_back/LocalE24StoreBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.oz.locale24_store_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalE24StoreBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
